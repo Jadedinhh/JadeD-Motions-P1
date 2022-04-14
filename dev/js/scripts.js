8e6b11b8e16798ee1ee634272c9b88d590c0c114
Jadedinhh <mbd77@drexel.edu>
2022-04-14 12:19:56 -0400

    playing with script

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,9 +2,10 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
+gsap.from("#orange-element", {duration:3, y:20});
+
 mainTL.from("#lieimg", {duration:5, x:30, opacity:0.5}, "img")
 .from("#phoneimg", {duration:5, x:-30, opacity: 0.3}, "img")
-.from("orange-element", {duration:5, y: 30}, "img")
 .from("#blueming", {duration:5, y:50, opacity: 0.5}, "img")
 
 
