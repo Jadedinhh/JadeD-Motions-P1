9095a13f72f1c2570e225162cd116bee8d62a211
Jadedinhh <mbd77@drexel.edu>
2022-04-19 12:07:22 -0400

    nice

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,6 +7,6 @@ gsap.from("#orange-element", {duration:3, y:20});
 mainTL.from("#lieimg", {duration:5, x:30, opacity:0.5}, "img")
 .from("#phoneimg", {duration:5, x:-30, opacity: 0.3}, "img")
 .from("#blueming", {duration:5, y:50, opacity: 0.5}, "img")
-.from(".dot", {duration: 2, transformOrigin: "left", skewX:0.5, scaleX:2, delay: 0.02, ease:"bounce.out"})
+.from(".dot", {duration: 1, transformOrigin: "left", skewX:0.5, scaleX:2, ease:"bounce.out"})
 
 ;
