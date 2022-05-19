2202035f3ec1c782ec0579b1772e08d50b243ef2
Jadedinhh <mbd77@drexel.edu>
2022-05-19 12:11:18 -0400

    working

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,7 +6,7 @@ import { MotionPathHelper } from "gsap/MotionPathHelper";
 
 gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin, MotionPathHelper);
 
-const mainTL= gsap.timeline({repeatDelay:0.5, repeat:3});
+const mainTL= gsap.timeline({repeat:3, repeatDelay:1});
 
 function changeBlue(){
 
@@ -14,7 +14,7 @@ function changeBlue(){
     tl.to("#flower", {duration:0.5, fill:"#1D4995"}, "start")
     .to("#petal1",{duration:0.5, y:"-80", x:"-20"})
     .to("#mouth", {duration:0.5, rotation:180, x:"100", y:"90"}, "start")
-    .to("#petal1", {duration: 0.5, y:"0", x:"0"});
+    .to("#petal1", {duration: 0.5, y:"0", x:"0", ease:"power1.out"});
 
 
     return tl;
@@ -27,7 +27,7 @@ function changePink(){
     tl.to("#flower", {duration:0.5, fill:"#F7B0BE"}, "go")
     .to("#petal2",{duration:0.5, y:"-60", x:"80"})
     .to("#mouth", {duration:0.5, rotation:360, x:"0", y:"0"}, "go")
-    .to("#petal2", {duration: 0.5, y:"0", x:"0"});
+    .to("#petal2", {duration: 0.5, y:"0", x:"0",ease:"power1.out"});
 
 
     return tl;
@@ -40,7 +40,7 @@ function changeOcean(){
     tl.to("#flower", {duration:0.5, fill:"#1F79AF"}, "run")
     .to("#petal3",{duration:0.5, y:"60", x:"80"})
     .to("#mouth", {duration:0.5, rotation:180, x:"100", y:"90"}, "run")
-    .to("#petal3", {duration: 0.5, y:"0", x:"0"});
+    .to("#petal3", {duration: 0.5, y:"0", x:"0", ease:"power1.out"});
 
 
     return tl;
@@ -51,7 +51,7 @@ function changeOrange(){
     tl.to("#flower", {duration:0.5, fill:"#F15B43"}, "let")
     .to("#petal4",{duration:0.5, y:"80", x:"-60"})
     .to("#mouth", {duration:0.5, rotation:360, x:"0", y:"0"}, "let")
-    .to("#petal4", {duration: 0.5, y:"0", x:"0"});
+    .to("#petal4", {duration: 0.5, y:"0", x:"0", ease:"power1.out"});
 
 
     return tl;
@@ -62,7 +62,7 @@ function changeMint(){
     tl.to("#flower", {duration:0.5, fill:"#D0E7CE"}, "move")
     .to("#petal5",{duration:0.5, y:"60", x:"-80"})
     .to("#mouth", {duration:0.5, rotation:180, x:"100", y:"90"}, "move")
-    .to("#petal5", {duration: 0.5, y:"0", x:"0"})
+    .to("#petal5", {duration: 0.5, y:"0", x:"0", ease:"power1.out"})
     .to("#flower", {duration:0.5, fill:"#F15B43"}, "restart")
     .to("#mouth", {duration:0.5, rotation:360, x:"0", y:"0"}, "restart");
     
